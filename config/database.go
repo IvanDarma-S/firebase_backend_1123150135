40b18c135a407d75da848c406d1394816c223ee6
IvanDarma-S <ivandarma3000@gmail.com>
2026-03-28 17:28:56 +0700

    membahkan kode di database.go

--- config/database.go
+++ config/database.go
@@ -8,3 +8,19 @@ import (
  "gorm.io/gorm"
  "gorm.io/gorm/logger"
 )
+// DB adalah instance GORM global yang dipakai di seluruh aplikasi
+var DB *gorm.DB
+func InitDatabase() {
+ // Ambil konfigurasi dari environment variables
+ host := os.Getenv("DB_HOST")
+ port := os.Getenv("DB_PORT")
+ user := os.Getenv("DB_USER")
+ password := os.Getenv("DB_PASSWORD")
+ dbname := os.Getenv("DB_NAME")
+ // Format DSN (Data Source Name) untuk MySQL
+ // Format: user:pass@tcp(host:port)/dbname?params
+ dsn := fmt.Sprintf(
+ "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+ user, password, host, port, dbname,
+ )
+}
